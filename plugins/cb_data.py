52bb00bfb4efa70a1d161d8494fc45194319a2b3
shadowXalone <146691704+shadowXalone@users.noreply.github.com>
2023-10-07 09:47:44 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -16,7 +16,7 @@ from helper.progress import humanbytes
 from helper.set import escape_invalid_curly_brackets
 import os
 
-log_channel = int(os.environ.get("LOG_CHANNEL", "-1001926802833"))
+log_channel = int(os.environ.get("LOG_CHANNEL", ""))
 
 API_ID = int(os.environ.get("API_ID", ""))
 
